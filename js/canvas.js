3dc4a58c8371a08206d46f0728603af41e00017b
Exception <39933347+missexception@users.noreply.github.com>
2019-04-28 21:04:49 +0800

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,11 @@
 var canvasWidth = 600
+if(window.innerWidth&&window.innerHeight){
+	if(window.innerHeight>window.innerWidth){
+		canvasWidth=window.innerWidth;
+	}else{
+		canvasWidth=window.innerHeight;
+	}
+}
 var canvasHeight = canvasWidth
 
 var isMousDown = false
